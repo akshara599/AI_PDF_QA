5a990f991d2ed42570bcc912771e936356670682
Akshara Kaushik <aksharakaushik4@gmail.com>
2026-06-26 16:56:58 +0530

    Updated main file

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,8 @@ app.add_middleware(
     CORSMiddleware,
 
     allow_origins=[
-        "http://localhost:5173"
+           "http://localhost:5173",
+            "https://your-frontend-name.vercel.app"
     ],
 
     allow_credentials=True,
